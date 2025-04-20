b11a36ed382faf7cdb46c820f82d0e8ab61e5fb4
UZD007 <djdh9515@gmail.com>
2025-04-20 17:59:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let ism = prompt("Ismingizni kiriting:")
+let harf = prompt("Birorta harf kiriting:")
+if(ism.includes(harf)){
+    alert(ism+" ismida " +harf+" harfi mavjud")
+}else{
+    alert(ism+" ismida " +harf+" harfi mavjud emas")
+}
